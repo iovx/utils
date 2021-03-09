f87f86fd007af6367b0b6ae8b09b60f2c3f17b1f
ixw2017@163.com <Wsrf@2017@git$>
2021-03-09 22:50:57 +0800

    chore: add codecov

--- src/helpers/__tests__/util.test.ts
+++ src/helpers/__tests__/util.test.ts
@@ -2,7 +2,7 @@ import {fixedNum, guid} from "../util";
 
 describe('基础工具', () => {
   test('test util fixNum', () => {
-    expect(fixedNum(1, 3)).toBe('0001');
+    expect(fixedNum(1, 3)).toBe('001');
   });
   test('guid generate', () => {
     expect(guid().length).toBe(36);
